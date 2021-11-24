eaac882acdd40068d96064606e81d7339580b408
Aditya Gupta <gupta.aditya@gmail.com>
2021-11-24 20:30:04 +0100

    restruct

--- first.hs
+++ first.hs
@@ -1,3 +1,12 @@
+main :: IO ()
+main = do
+  putStrLn "What is 2 + 2?"
+  x <- readLn
+  if x == 4
+    then putStrLn "You're right!"
+    else putStrLn "You're wrong!"
+
+
 funcN :: Integral a => a -> a
 funcN n =
   if even n
@@ -7,10 +16,3 @@ funcN n =
 doubleDouble :: Num a => a -> a
 doubleDouble x = (\dubs -> dubs * 2) (x * 2)
 
-main :: IO ()
-main = do
-  putStrLn "What is 2 + 2?"
-  x <- readLn
-  if x == 4
-    then putStrLn "You're right!"
-    else putStrLn "You're wrong!"
