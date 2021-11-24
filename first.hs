8b1f04a0bc31e954120e21d73055659c590f2b8c
Aditya Gupta <gupta.aditya@gmail.com>
2021-11-24 21:02:45 +0100

    chapter 4.1

--- first.hs
+++ first.hs
@@ -1,3 +1,13 @@
+import           Data.List
+import Data.Binary (Binary(putList))
+
+names = [ ("Ian", "Curtis")
+        , ("Bernard", "Sumner")
+        , ("Peter", "Hook")
+        , ("Stephen", "Morris")]
+
+
+
 main :: IO ()
 main = do
   putStrLn "What is 2 + 2?"
@@ -6,13 +16,10 @@ main = do
     then putStrLn "You're right!"
     else putStrLn "You're wrong!"
 
-
 funcN :: Integral a => a -> a
-funcN n =
-  if even n
-    then n - 2
-    else 3 * n + 1
+funcN n = if even n
+          then n - 2
+          else 3 * n + 1
 
 doubleDouble :: Num a => a -> a
 doubleDouble x = (\dubs -> dubs * 2) (x * 2)
-
