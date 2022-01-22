6f7e0f7323d1d20cdad8978638e7fce2c3f0c855
Aditya Gupta <gupta.aditya@gmail.com>
2022-01-22 21:29:03 +0100

    new func even odd

--- first.hs
+++ first.hs
@@ -1,5 +1,5 @@
 import           Data.List
-import Data.Binary (Binary(putList))
+import           Data.Binary (Binary(putList))
 
 names = [ ("Ian", "Curtis")
         , ("Bernard", "Sumner")
@@ -19,6 +19,11 @@ funcN :: Integral a => a -> a
 funcN n = if even n
           then n - 2
           else 3 * n + 1
+
+funcD :: Integral a => a -> a
+funcD d = if even d
+          then d + 2
+          else d + 3
 
 doubleDouble :: Num a => a -> a
 doubleDouble x = (\dubs -> dubs * 2) (x * 2)
